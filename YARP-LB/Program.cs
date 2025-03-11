2848eec0473552029aa7067cdf2166d26affe9ab
manishchopra@myself.com <manishchopra@myself.com>
2025-03-11 19:06:54 +0000

    wip

--- YARP-LB/Program.cs
+++ YARP-LB/Program.cs
@@ -1,11 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy()
-    .LoadFromConfig(builder.Configuration.GetSection("YARP"));
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
 
-app.MapGet("/api", () => Results.Ok("Hello from YARP"));
+app.MapGet("/", () => Results.Ok("Hello from YARP"));
 
 app.MapReverseProxy();
 
